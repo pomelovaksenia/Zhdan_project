7d13c9d6e2f62efd83450f72f642772cb47afe69
pomelovaksenia <skorpioshka2004@gmail.com>
2023-07-13 22:42:29 +0300

    мини обнова 4

--- src/main/java/system_pac/model/Comments.java
+++ src/main/java/system_pac/model/Comments.java
@@ -3,6 +3,19 @@ package system_pac.model;
 import java.util.Date;
 
 public class Comments {
+    public Date getCommentDate() {
+        return commentDate;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public Comments(Date commentDate, String commentText) {
+        this.commentDate = commentDate;
+        this.commentText = commentText;
+    }
+
     private Date commentDate;
     private String commentText;
 
