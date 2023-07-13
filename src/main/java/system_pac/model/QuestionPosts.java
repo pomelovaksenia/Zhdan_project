5feea848358d7126a092dbe7d0fe1550d5791dc2
Irina <116427830+11Rin@users.noreply.github.com>
2023-07-13 22:41:03 +0300

    Update .gitignore

--- src/main/java/system_pac/model/QuestionPosts.java
+++ src/main/java/system_pac/model/QuestionPosts.java
@@ -1,4 +1,27 @@
 package system_pac.model;
 
+import javax.xml.crypto.Data;
+
 public class QuestionPosts {
+    String answer;
+    UserAccount account;
+    Data data;
+
+    public QuestionPosts(String answer, UserAccount account, Data data) {
+        this.answer = answer;
+        this.account = account;
+        this.data = data;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public UserAccount getAccount() {
+        return account;
+    }
+
+    public Data getData() {
+        return data;
+    }
 }
