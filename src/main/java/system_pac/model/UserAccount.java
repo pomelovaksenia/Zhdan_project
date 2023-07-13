cebdcfac6ca1738ce3d2494b4a0ef4457203da96
pomelovaksenia <skorpioshka2004@gmail.com>
2023-07-13 22:24:54 +0300

    мини обнова 2

--- src/main/java/system_pac/model/UserAccount.java
+++ src/main/java/system_pac/model/UserAccount.java
@@ -1,4 +1,25 @@
 package system_pac.model;
 
 public class UserAccount {
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getShift() {
+        return shift;
+    }
+
+    public UserAccount(String name, String surname, Integer shift) {
+        this.name = name;
+        this.surname = surname;
+        this.shift = shift;
+    }
+
+    private String name;
+    private String surname;
+    private Integer shift;
 }
